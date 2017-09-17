014a11b045fe9ebbb412843d03a8d17767956c09
pab <pablo.l.balbi@gmail.com>
2017-09-17 17:20:39 -0300

    3-a listo

--- aephe.py
+++ aephe.py
@@ -17,7 +17,24 @@ def AEPHE(img, N, alpha=1./3., beta=1./3., gamma=1./3.):
         # previo_a_3 : TODO: Computar M_i y M_c según el paper, los cuales son los parámetros alpha y beta
     # 3 : Aplicar HE a cada histrograma particionado según el paper:
     for i in range(0,N): # para cada partición del histograma
+
+        # IMPORTANTE:
+        # parts_histo[i] esta en float, pero sus valores son en int, hay que
+        # normalizarlo antes de calcular el target_histo
+
         # a : Crear el histograma uniforme según la función de peso
+        # w_k sería la funcion peso para generar el histograma uniforme
+        w_k = weight_function(parts_histo[i])
+        histo_unif = np.zeros(256) # esta en float
+        for j in range(0,256):
+            if parts_histo[i][j]>0.1: # si no es cero, quiere decir que el valor esta en
+            # esta parte del histo
+                histo_unif[j] = 1
+            else:
+                # el valor debe ser 0, es decir, no esta en la parte,
+                # aplico weight_function
+                histo_unif[j] = w_k(j)
+                
         # b : Resolver el sistema lineal, para hallar el target_histogram
     # 4 : Juntar los histogramas una vez ecualizados, por el peso
     # 5 : Obtener el canal-I final, por HM
@@ -46,3 +63,37 @@ def split_extend_histo(histo, N):
             histo_parts[i][j] = histo[j]
     return histo_parts
 
+# calcula en base al histograma, la funcion de peso para generar el histo_unif
+# adaptado a la particion
+def weight_function(piece_histo):
+    # esta parte esta en pag. 1014 del paper, secc 3.3
+    # u_k
+    first_val = 0
+    first_found = False
+    last_val = 0
+    # para hallar el valor medio del intervalo
+    count = 0
+    mean = 0.
+    # busco el primer y el ultimo valor de la particion
+    # siempre se cumple last_val > first_val
+    for i in range(0,256):
+        # como se que son float, pero en relidad enteros, evaluo mayor a 0.1
+        if piece_histo[i] > 0.1:
+            if not first_found:
+                first_val = i
+                first_found = True
+            else:
+                last_val = i
+            count = count+1
+            mean = mean+i
+    # saco la media, eso va a ser u_k
+    u_k = (first_val + last_val)/2.
+    # sigma_k
+    mean = mean/float(count)
+    # sugerido por paper
+    sigma_th = np.abs(128-mean)
+    # cantidad de valores que cubre el intervalo
+    w_d = last_val - first_val
+    # tomo el maximo
+    sigma_k = np.amax([w_d,sigma_th])
+    return lambda i: np.exp(-((i - u_k)**2)/(2*(sigma_k**2)))
