d0c1084fc95e6688850493a62d5d9cb611187da6
npazosmendez <npazosmendez@gmail.com>
2017-09-17 19:04:33 -0300

    listo HM(img,histo)

--- images.py
+++ images.py
@@ -13,7 +13,37 @@ def get_histo(img):
     return h
 
 def HM(img, histo):
-    # podria ser in-place
+    assert len(histo)==L
+
+    # guardo las frecuencias absolutas de la imagen para calcular la acumulada
+    acum_r = np.zeros(L)
+    for f in range(len(img)):
+        for j in range(len(img[f])):
+            acum_r[img[f][j]]+=1
+
+    # calculo las dos acumuladas, la entrada r, la normal s
+    acum_r[0] /= len(img)*len(img[0])
+    for i in range(1,L):
+        # calculo las frecuencias relativas, obteniendo la distribucion
+        acum_r[i] /= len(img)*len(img[0])
+        # acumulo
+        acum_r[i] += acum_r[i-1]
+        histo[i] += histo[i-1]
+
+    # calculo las imagenes de los valores 0...255 en la funcion T(r)
+    # la salida s se calcula como s=T(r)=F_s^-1(F_r(r)).
+    # al trabajar con valores discretos, se busca el valor mayor inmediato.
+    T_r = np.zeros(L)
+    for p in range(L):
+        acum = acum_r[p]
+        k = 0
+        while histo[k]<acum and k<255:
+            k+=1
+        T_r[p] = k
+
+    # construyo la imagen de salida
+    for f in range(len(img)):
+        for j in range(len(img[f])):
+            img[f][j] = T_r[img[f][j]]
 
     return img
-
