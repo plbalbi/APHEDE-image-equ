e2254c1167c73cf9cdec880f406988238d6608e1
pab <pablo.l.balbi@gmail.com>
2017-09-19 12:59:50 -0300

    cambio aridad. cambio imshow
            - Ahora se le pasa solo el N y la ruta donde guardar la imagen
            - Al hacer el show, corre el split normal, y el acum

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import aephe
 from scipy import ndimage
+from scipy import misc
 from matplotlib import pyplot as plt
 import sys
 
 if len(sys.argv) < 2:
     print("Error, faltan argumentos.")
-    print("%s imagen [N=3] [alpha=.6] [beta=.2] [gamma=0]" % (sys.argv[0]))
+    print("%s imagen salida [N=3] " % (sys.argv[0]))
+    # print("%s imagen salida [N=3] [alpha=.6] [beta=.2] [gamma=0]" % (sys.argv[0]))
     sys.exit(1)
 
 route = sys.argv[1]
@@ -18,16 +20,24 @@ b = .5
 g = 0.
 
 if len(sys.argv) > 2:
-    N = int(sys.argv[2])
-    a = float(sys.argv[3])
-    b = float(sys.argv[4])
-    g = float(sys.argv[5])
+    out_name = sys.argv[2]
+    N = int(sys.argv[3])
+    # N = int(sys.argv[2])
+    # a = float(sys.argv[3])
+    # b = float(sys.argv[4])
+    # g = float(sys.argv[5])
 
 print("Running AEPHE...")
-img_AEPHE = aephe.AEPHE(img, N, alpha = a, beta = b, gamma = g, splits = [100,200])
+img_AEPHE = aephe.AEPHE(img, N, alpha = a, beta = b, gamma = g)
+print("Running AEPHE with acum_split...")
+img_AEPHE_acum_splits = aephe.AEPHE(img, N, alpha = a, beta = b, gamma = g, acum_split=True)
 plt.clf()
-plt.subplot(1,2,1)
+plt.subplot(1,3,1)
 plt.imshow(img)
-plt.subplot(1,2,2)
+plt.subplot(1,3,2)
 plt.imshow(img_AEPHE)
+misc.imsave("out/%s_AEPHE.bmp" % (out_name), img_AEPHE)
+plt.subplot(1,3,3)
+plt.imshow(img_AEPHE_acum_splits)
+misc.imsave("out/%s_AEPHE_AS.bmp" % (out_name), img_AEPHE_acum_splits)
 plt.show()
